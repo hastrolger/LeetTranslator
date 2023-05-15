0d9bc6a0573621d05d664be0328d36fe63967a24
hastrolger <parralesjael@gmail.com>
2023-05-15 15:19:10 -0700

    Function to translate from human lang to leet lang

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Dictionary<String, String> leetCorrespondence = new Dictionary<string, string>(){
+    {"A", "4"},
+    {"B", "I3"},
+    {"C", "["},
+    {"D", ")"},
+    {"E", "3"},
+    {"F", "|="},
+    {"G", "&"},
+    {"H", "#"},
+    {"I", "1"},
+    {"J", ",_|"},
+    {"K", ">|"},
+    {"L", "1"},
+    {"M", @"/\/\"},
+    {"N", "^/"},
+	{"Ñ", "+N" },
+    {"O", "0"},
+    {"P", "|*"},
+    {"Q", "(_,)"},
+    {"R", "I2"},
+    {"S", "5"},
+    {"T", "7"},
+    {"U", "(_)"},
+    {"V", @"\/"},
+    {"W", @"\/\/"},
+    {"X", "><"},
+    {"Y", "j"},
+    {"Z", "2"}
+};
+
+Console.WriteLine("Leet Translator");
+string translatedString = LeetTranslator("ho la", leetCorrespondence);
+Console.WriteLine(translatedString);
+
+string LeetTranslator(string originalString, Dictionary<String,String> leetAlphabet) {
+    string translatedString = "";
+
+    foreach(char c in originalString) {
+        if(leetAlphabet.ContainsKey(c.ToString().ToUpper())){
+            translatedString += leetAlphabet[c.ToString().ToUpper()];
+        } else {
+            translatedString += c;
+        }
+    }
+
+    return translatedString;
+}
